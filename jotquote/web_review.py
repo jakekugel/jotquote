6ae6e53d1b088f8ff181d2e6a87deea11d064db1
Jake Kugel <jake_kugel@yahoo.com>
2026-03-15 20:35:12 -0500

    Minor fixes

--- jotquote/web_review.py
+++ jotquote/web_review.py
@@ -10,31 +10,31 @@ from jotquote import api
 
 app = Flask(__name__)
 
-STAR_TAGS = ['1star', '2stars', '3stars', '4stars', '5stars']
-VISIBILITY_TAGS = ['personal', 'family', 'public']
+STAR_TAGS = ["1star", "2stars", "3stars", "4stars", "5stars"]
+VISIBILITY_TAGS = ["personal", "family", "public"]
 
 
-@app.route('/')
+@app.route("/")
 def index():
     config = api.get_config()
-    quotefile = config.get(api.APP_NAME, 'quote_file')
-    page_title = config.get(api.APP_NAME, 'web_page_title', fallback='jotquote')
-    show_stars = config[api.APP_NAME].get('web_show_stars', 'false').lower() == 'true'
+    quotefile = config.get(api.APP_NAME, "quote_file")
+    page_title = config.get(api.APP_NAME, "web_page_title", fallback="jotquote")
+    show_stars = config[api.APP_NAME].get("web_show_stars", "false").lower() == "true"
 
     quotes = api.read_quotes(quotefile)
-    quote = api.get_first_match(quotes, excluded_tags=','.join(STAR_TAGS), rand=True)
+    quote = api.get_first_match(quotes, excluded_tags=",".join(STAR_TAGS), rand=False)
 
     if quote is None:
         return "<p>No matching quote found.</p>", 200
 
     quote_tags_set = set(quote.tags)
-    star_tag = next((t for t in STAR_TAGS if t in quote_tags_set), '')
-    visibility_tag = next((t for t in VISIBILITY_TAGS if t in quote_tags_set), '')
+    star_tag = next((t for t in STAR_TAGS if t in quote_tags_set), "")
+    visibility_tag = next((t for t in VISIBILITY_TAGS if t in quote_tags_set), "")
     other_tags = sorted(t for t in quote_tags_set if t not in STAR_TAGS and t not in VISIBILITY_TAGS)
     date1 = datetime.datetime.now().strftime("%A, %B %d, %Y")
 
     return render_template(
-        'review.html',
+        "review.html",
         quote=quote.quote,
         author=quote.author,
         publication=quote.publication,
@@ -47,28 +47,28 @@ def index():
         visibility_tags=VISIBILITY_TAGS,
         star_tag=star_tag,
         visibility_tag=visibility_tag,
-        other_tags='\n'.join(other_tags),
+        other_tags="\n".join(other_tags),
     )
 
 
-@app.route('/settags', methods=['POST'])
+@app.route("/settags", methods=["POST"])
 def settags():
     config = api.get_config()
-    quotefile = config.get(api.APP_NAME, 'quote_file')
-    hash_val = request.form.get('hash')
-    star_tag = request.form.get('star_tag', '')
-    visibility_tag = request.form.get('visibility_tag', '')
-    other_tags_raw = request.form.get('other_tags', '')
+    quotefile = config.get(api.APP_NAME, "quote_file")
+    hash_val = request.form.get("hash")
+    star_tag = request.form.get("star_tag", "")
+    visibility_tag = request.form.get("visibility_tag", "")
+    other_tags_raw = request.form.get("other_tags", "")
 
     newtags = []
     if star_tag:
         newtags.append(star_tag)
     if visibility_tag:
         newtags.append(visibility_tag)
-    for part in other_tags_raw.replace(',', '\n').split('\n'):
+    for part in other_tags_raw.replace(",", "\n").split("\n"):
         tag = part.strip()
         if tag:
             newtags.append(tag)
 
     api.settags(quotefile, n=None, hash=hash_val, newtags=newtags)
-    return redirect('/')
+    return redirect("/")
