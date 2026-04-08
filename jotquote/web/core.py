fde734f7d3cbddfa7132f9c5f179af561be9aea4
Jake Kugel <jakekugel@users.noreply.github.com>
2026-04-08 16:54:04 -0500

    Feature/log timestamp (#29)
    * Log timestamp
    
    * Logging timestamp

--- jotquote/web/core.py
+++ jotquote/web/core.py
@@ -2,9 +2,49 @@
 #  This file is licensed under the terms of the MIT License.  See the LICENSE
 # file in the root of this repository for complete details.
 
+import logging
+import time
+
 from jotquote import api
 
-LOG_FORMAT = '%(levelname)s %(name)s:%(message)s'
+LOG_FORMAT = '%(asctime)s %(levelname)s %(name)s:%(message)s'
+
+
+def abbreviate_timezone(name):
+    """Return an abbreviation for a timezone name by taking the first letter of each word.
+
+    If the name is already short (e.g. 'CDT', 'EST'), it is returned unchanged.
+    Multi-word names like 'Central Daylight Time' become 'CDT'.
+
+    :param name: str timezone name
+    :return: str abbreviated timezone name
+    """
+    if not name:
+        return name
+    words = name.split()
+    if len(words) <= 1:
+        return name
+    return ''.join(w[0] for w in words)
+
+
+class TimestampFormatter(logging.Formatter):
+    """Formatter that produces timestamps like '2026/04/07 06:21:42.023 AM CDT'."""
+
+    def formatTime(self, record, datefmt=None):  # noqa: N802
+        """Return a formatted timestamp string with milliseconds, AM/PM, and timezone."""
+        ct = self.converter(record.created)
+        t = time.strftime('%Y/%m/%d %I:%M:%S', ct)
+        tz = abbreviate_timezone(time.strftime('%Z', ct))
+        return '{}.{:03d} {} {}'.format(t, int(record.msecs), time.strftime('%p', ct), tz)
+
+
+def configure_logging():
+    """Set up the root logger with TimestampFormatter if not already configured."""
+    if not logging.root.handlers:
+        handler = logging.StreamHandler()
+        handler.setFormatter(TimestampFormatter(LOG_FORMAT))
+        logging.root.setLevel(logging.INFO)
+        logging.root.addHandler(handler)
 
 
 def sanitize_for_log(value):
